19bc7d3bc5dbf1b477b4b39deac1314e7e1e708b
MichielMAnalytics <135761097+MichielMAnalytics@users.noreply.github.com>
2025-06-21 15:54:42 +0200

    minor fix

--- packages/api/src/utils/index.ts
+++ packages/api/src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './azure';
 export * from './common';
 export * from './events';
+export * from './files';
 export * from './generators';
 export { default as Tokenizer } from './tokenizer';
